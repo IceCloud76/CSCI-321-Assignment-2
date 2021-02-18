790fcc1a66cfdcd27cad7c0b66c9509e488f6587
Eric <starwolf64.exe@gmail.com>
2021-02-18 15:41:27 -0600

    Check for empty textbook working.

--- Assignment2/ViewController.swift
+++ Assignment2/ViewController.swift
@@ -12,19 +12,37 @@ class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate 
     @IBOutlet weak var labelTwo: UILabel!
     @IBOutlet weak var labelThree: UILabel!
     @IBOutlet weak var labelFour: UILabel!
-    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var stepperOne: UIStepper!
     @IBOutlet weak var stepperTwo: UIStepper!
+    @IBOutlet weak var myTextField: UITextField!
+    
     
     var bill_amount: CGFloat = CGFloat(0.00)
     var tip_percentage: Int = 20
     var party_size: Int = 1
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func buttonPressed(){
-        
+
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+            guard let firstNumberString = myTextField.text, !firstNumberString.isEmpty, let firstNumber = Int(firstNumberString) else {
+            // print("Error - invalid first number")
+            displayError("Please enter a valid first number", textField: myTextField)
+            return
+        }
     }
+    
+    
+    func displayError(_ message: String, textField: UITextField) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true) {
+            textField.text = ""
+        }
+    }
+    
 }
