9e7fbf445c11d19fb4b218a771271d0c0cf52f55
Daniel Hufford <danhufford00@gmail.com>
2021-02-18 12:13:17 -0800

    Starting work on ViewController

--- Assignment2/ViewController.swift
+++ Assignment2/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTwo: UILabel!
+    @IBOutlet weak var labelThree: UILabel!
+    @IBOutlet weak var labelFour: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var stepperOne: UIStepper!
+    @IBOutlet weak var stepperTwo: UIStepper!
+    
+    var bill_amount: CGFloat = CGFloat(0.00)
+    var tip_percentage: Int = 20
+    var party_size: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func buttonPressed(){
+        
+    }
 }
-
